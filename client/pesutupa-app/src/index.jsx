bf47a3dfa411faf19fdb8434485f74da26b59ecc
Laura Koivu <angelxnights@gmail.com>
2021-12-03 23:42:54 +0200

    Vaihdettu polkua, koska tyylit-kansio luotu

--- client/pesutupa-app/src/index.jsx
+++ client/pesutupa-app/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./tyylit/index.css";
+import "./tyylit/kalenteri.css";
 import App from "./App";
 import { UserProvider } from "./context/UserContext";
 import theme from "./theme";
