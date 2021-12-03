c82b8cdcf7cda7a74975b95df7a4bf24e90ad797
Laura Koivu <angelxnights@gmail.com>
2021-12-03 23:43:25 +0200

    Meillä on kalenteri, jota lähteä työstämään!
    https://github.com/jquense/react-big-calendar

--- client/pesutupa-app/src/pages/UusiVaraus.jsx
+++ client/pesutupa-app/src/pages/UusiVaraus.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Kalenteri from "../components/kalenteri/Kalenteri";
 
 const UusiVaraus = () => {
   return (
     <div>
       <h1>uusi varaus</h1>
+      <Kalenteri />
     </div>
   );
 };
